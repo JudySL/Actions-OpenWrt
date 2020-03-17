431a08c446d5e641f334be978e9915afad433c16
JudySL <58675509+JudySL@users.noreply.github.com>
2020-03-17 22:45:11 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -17,3 +17,5 @@ git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
 git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
 #git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
 #git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
+./scripts/feeds update -a
+./scripts/feeds install -a
