49940df14cf18d693b9159af50cca94cb0d5b0da
JudySL <58675509+JudySL@users.noreply.github.com>
2020-03-17 22:21:05 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -5,5 +5,15 @@
 # Author: P3TERX
 # Blog: https://p3terx.com
 #=================================================
-# Modify default IP 修改默认IP
+
+# 定制默认IP
 sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+# 替换默认Argon主题
+rm -rf package/lean/luci-theme-argon
+git clone https://github.com/jerrykuku/luci-theme-argon package/luci-theme-argon
+
+# 添加第三方软件包
+git clone https://github.com/tty228/luci-app-serverchan package/luci-app-serverchan
+#git clone https://github.com/rufengsuixing/luci-app-adguardhome package/luci-app-adguardhome
+#git clone https://github.com/kang-mk/luci-app-smartinfo package/luci-app-smartinfo
